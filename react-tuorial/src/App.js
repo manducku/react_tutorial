8e9662bb1a0ceff5260fa129b61abfd4594e7699
manducku <alwaysfun2183@gmail.com>
2016-05-10 18:33:42 +0900

    jsx example

--- react-tuorial/src/App.js
+++ react-tuorial/src/App.js
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    _sayHey(){
+        alert("hey");
+    }
     render(){
+    let text = "Dev-Server"
+    let pStyle = {
+        color: 'red',
+        backgroundColor: 'black',
+    }
+
         return (
-                <h1>Hello World </h1>
+                <div>
+                    <h1>Hello World</h1>
+                    <h2> welcome to {text} </h2>
+                    <button onClick={this._sayHey}>Click Me</button>
+                    <p style = {pStyle}>{{text}? 'True' : 'False'}
+                    </p>
+                </div>
                );
     }
 }
