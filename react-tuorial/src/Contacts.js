875c671d82987402e0edfad1878e6a90ebb4801e
manducku <alwaysfun2183@gmail.com>
2016-05-12 13:41:45 +0900

    add Comment feature

--- react-tuorial/src/Contacts.js
+++ react-tuorial/src/Contacts.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import update from 'react-addons-update';
 
 
 class Contacts extends React.Component{
@@ -12,6 +13,15 @@ class Contacts extends React.Component{
                                 ] 
                     }
     }
+
+    _insertContact(name, phonenumber){
+        let newState = update(this.state, {
+            contactData: {
+                    $push: [{name:name, phonenumber: phonenumber}]
+            }
+        });
+        this.setState(newState);
+    }
     render(){
         return(
             <div>
@@ -19,19 +29,60 @@ class Contacts extends React.Component{
                     {this.state.contactData.map((contactData, i) =>
                                                 {return (<ContactInfo name={contactData.name} phonenumber={contactData.phonenumber} key={i} />
                                                 );
-                                                })}
+                                         })}
                 </ul>
+                <ContactCreate onInsert={this._insertContact.bind(this)}/>
             </div>
              );
     }
 }
 
 class ContactInfo extends React.Component{
-    render(){
+        render(){
         return (
             <li> {this.props.name} {this.props.phonenumber} </li>
             );
             }
+}
+
+class ContactCreate extends React.Component{
+
+    constructor(props){
+            super(props);
+            this.state = {
+                name: "",
+                phonenumber: ""
+            };
+        }
+    
+    handleChange(e){
+        var nextState = {};
+        nextState[e.target.name]= e.target.value;
+        console.log(e.target.name, e.target.value);
+        this.setState(nextState);
+    }
+
+    handleClick(e){
+        this.props.onInsert(this.state.name, this.state.phonenumber);
+        this.setState({
+            name:"",
+            phonenumber:""
+        });
+    }
+
+    render(){
+            return ( 
+                <div>
+                    <p>
+                        <input type="text" name="name" placeholder="name" value={this.state.name} onChange={this.handleChange.bind(this)}/>
+                        <input type="text" name="phonenumber" placeholder="phonenumber" value={this.state.phonenumber} onChange={this.handleChange.bind(this)}/>
+                        <button onClick={this.handleClick.bind(this)}>
+                            Insert
+                        </button>
+                    </p> 
+                </div>
+            );
+    }
 }
 
 export default Contacts;
