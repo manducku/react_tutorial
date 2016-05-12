51db56a9a4db760d1f15b341a943410896a1f7ef
manducku <alwaysfun2183@gmail.com>
2016-05-12 17:19:09 +0900

    add modified comment feature

--- react-tuorial/src/Contacts.js
+++ react-tuorial/src/Contacts.js
@@ -11,7 +11,10 @@ class Contacts extends React.Component{
                             {name:"hanseam", phonenumber:"01054222183"},
                             {name:"yoonjung", phonenumber:"01078853333"}
                                 ],
-                    selectedKey: -1
+                    selectedKey: -1,
+                    selectedContact: {
+                        name:"", phonenumber:""
+                        }
                     }
     }
 
@@ -34,7 +37,8 @@ class Contacts extends React.Component{
         }
         else{
             this.setState({
-                selectedKey: contactKey
+                selectedKey: contactKey,
+                selectedContact: this.state.contactData[contactKey]
             });
             console.log(contactKey+"is selected");
         }
@@ -51,6 +55,45 @@ class Contacts extends React.Component{
             return false;
         }
     }
+    
+    _removeContact(){
+        if(this.state.selectedKey === -1){
+            alert("not selected");
+            return;
+        }
+        else{
+            this.setState({
+                contactData: update(
+                    this.state.contactData,
+                    {
+                        $splice: [[this.state.selectedKey, 1]]
+                    }
+                ),
+                selectedKey: -1
+                });
+        }
+    }
+    
+    _modifiedContact(name, phonenumber){
+        if(this.state.selectedKey === -1){
+            alert("not selected");
+            return;
+        }
+        else{
+            this.setState({
+                contactData: update(
+                    this.state.contactData,
+                    {
+                        [this.state.selectedKey]:{
+                                name:{ $set: name},
+                                phonenumber:{ $set: phonenumber}
+                            }
+                    }
+                )
+            });
+        }
+    }
+
 
     render(){
         return(
@@ -68,6 +111,8 @@ class Contacts extends React.Component{
                                          })}
                 </ul>
                 <ContactCreate onInsert={this._insertContact.bind(this)}/>
+                <ContactRemove onRemove={this._removeContact.bind(this)}/>
+                <ContactModified onModified={this._modifiedContact.bind(this)} contact={this.state.selectedContact}/>
             </div>
              );
     }
@@ -136,6 +181,58 @@ class ContactCreate extends React.Component{
                     </p> 
                 </div>
             );
+    }
+}
+
+class ContactRemove extends React.Component{
+
+    handleClick(){
+        this.props.onRemove();
+    }
+
+    render(){
+        return (
+            <div>
+                <button onClick={this.handleClick.bind(this)} >Remove Comment </button>
+            </div>
+        );
+    }
+}
+
+class ContactModified extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            name:"",
+            phonenumber:"",
+        };
+    }
+    
+    handleClick(){
+        this.props.onModified(this.state.name, this.state.phonenumber);
+    }
+
+    handleChange(e){
+        let newState = {};
+        newState[e.target.name]=e.target.value;
+        this.setState(newState);
+    }
+    
+    componentWillReceiveProps(nextProps){
+        this.setState({
+            name: nextProps.contact.name,
+            phonenumber: nextProps.contact.phonenumber
+        });
+    }
+    
+    render(){
+        return (
+            <div>
+                <input type="text" name="name" placeholder="name" value={this.state.name} onChange={this.handleChange.bind(this)}/>
+                <input type="text" name="phonenumber" placeholder="placeholder" value={this.state.phonenumber} onChange={this.handleChange.bind(this)}/>
+                <button onClick={this.handleClick.bind(this)}>modified Comment </button>
+            </div>
+        );
     }
 }
 
